0beb680e404d29dbb773d144a92fb5ffa3d776ef
Yenci <a278862002@gmail.com>
2024-07-24 19:14:27 +0800

    Initial Commit

---
+++ IBSegueActionForLesson8Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBSegueActionForLesson8Practice
+//
+//  Created by 陳妍慈 on 2024/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
